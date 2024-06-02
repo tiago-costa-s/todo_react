7a69b69cd7bad16dbbfa33ab8318a6416aaa8bcb
tiago-costa-s <tiagocostasantos0013@gmail.com>
2024-06-02 10:02:50 -0300

    Inserido a tag button ao componete os icones foram envolvidos ao botão. A execução das funções sairam dos incones e foram para o botão

--- src/assets/components/todo/Todo.jsx
+++ src/assets/components/todo/Todo.jsx
@@ -11,13 +11,17 @@ const Todo = ({ todo, name, removeTodo }) => {
             <div className={styles.container}>
                 <p className={styles.title}>{name}</p>
                 <div className={styles.control_actions}>
-                    <IoIosCheckmarkCircleOutline
-                        className={styles.icons_todo}
-                    />
-                    <IoIosRemoveCircleOutline
-                        className={styles.icons_todo}
+                    <button
+                        className={styles.btn}
+                    >
+                        <IoIosCheckmarkCircleOutline className={styles.icons_todo} />
+                    </button>
+                    <button
+                        className={styles.btn}
                         onClick={() => removeTodo(todo.id)}
-                    />
+                    >
+                        <IoIosRemoveCircleOutline className={styles.icons_todo} />
+                    </button>
                 </div>
             </div>
             <div className={styles.date}>
